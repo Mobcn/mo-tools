1ac0c1515f3b8a8b8c9bba2283c0012ac96c06da
Mobcn <124491015+Mobcn@users.noreply.github.com>
2024-07-29 16:54:01 +0800

    Update import.js

--- page/sql/import.js
+++ page/sql/import.js
@@ -8,13 +8,13 @@
             // AlaSQL
             'https://npm.elemecdn.com/alasql@3.1.0/dist/alasql.min.js',
             // Monaco Editor
-            'https://cdn.staticfile.org/monaco-editor/0.36.1/min/vs/loader.min.js',
+            'https://cdn.staticfile.net/monaco-editor/0.36.1/min/vs/loader.min.js',
         ],
         css: [
             // Normalize
-            'https://cdn.staticfile.org/normalize/8.0.1/normalize.min.css',
+            'https://cdn.staticfile.net/normalize/8.0.1/normalize.min.css',
             // Element Plus样式
-            'https://cdn.staticfile.org/element-plus/2.2.18/index.css'
+            'https://cdn.staticfile.net/element-plus/2.2.18/index.css'
         ]
     },
 
@@ -24,11 +24,11 @@
         'vue3-sfc-loader': 'https://npm.elemecdn.com/vue3-sfc-loader@0.8.4/dist/vue3-sfc-loader.esm.js',
 
         // Vue3
-        vue: 'https://cdn.staticfile.org/vue/3.2.37/vue.esm-browser.prod.min.js',
+        vue: 'https://cdn.staticfile.net/vue/3.2.37/vue.esm-browser.prod.min.js',
 
         // Element Plus
-        'element-plus': 'https://cdn.staticfile.org/element-plus/2.2.18/index.full.min.mjs',
+        'element-plus': 'https://cdn.staticfile.net/element-plus/2.2.18/index.full.min.mjs',
         'element-plus/': 'https://npm.elemecdn.com/element-plus@2.2.18/',
-        '@element-plus/icons-vue': 'https://cdn.staticfile.org/element-plus-icons-vue/2.0.10/index.min.js'
+        '@element-plus/icons-vue': 'https://cdn.staticfile.net/element-plus-icons-vue/2.0.10/index.min.js'
     }
 }))();
