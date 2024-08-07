be7a7637eac46123d0a6e79a3a0aad7ca8fcfa43
Mobcn <124491015+Mobcn@users.noreply.github.com>
2024-08-07 15:13:07 +0800

    Update import.js

--- page/sql/import.js
+++ page/sql/import.js
@@ -21,7 +21,7 @@
     // ESM setting
     importToESM: {
         // Vue3 SFC Loader
-        'vue3-sfc-loader': 'https://npm.elemecdn.com/vue3-sfc-loader@0.8.4/dist/vue3-sfc-loader.esm.js',
+        'vue3-sfc-loader': 'https://unpkg.zhihu.com/vue3-sfc-loader@0.8.4/dist/vue3-sfc-loader.esm.js',
 
         // Vue3
         vue: 'https://cdn.staticfile.net/vue/3.2.37/vue.esm-browser.prod.min.js',
